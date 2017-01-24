c28e5620567feae70a975f2ab66bf17fb6ac55f3
wolfria <wolfria0606@gmail.com>
2017-01-24 23:02:32 +0900

    Project's Initial Commit

---
+++ www/js/javascript.js
@@ -0,0 +1,26 @@
+ons.ready(function() {  
+  var index = myTabbar.getActiveTabIndex();
+});
+
+function reloadimg(){
+    if (myTabbar.getActiveTabIndex() == 0) {
+        gazo0.src = "test.jpg?r=" + Math.random();
+//        gazo0.src = "http://192.168.0.150/speed0?r=" + Math.random();
+        console.log("停止");
+    } else if (myTabbar.getActiveTabIndex() == 1) {
+        gazo1.src = "test.jpg?r=" + Math.random();
+//        gazo1.src = "http://192.168.0.150/speed1?r=" + Math.random();
+        console.log("低速");
+    } else if (myTabbar.getActiveTabIndex() == 2) {
+        gazo2.src = "test.jpg?r=" + Math.random();
+//        gazo2.src = "http://192.168.0.150/speed2?r=" + Math.random();
+        console.log("中速");
+    } else{
+        gazo3.src = "test.jpg?r=" + Math.random();
+//        gazo3.src = "http://192.168.0.150/speed3?r=" + Math.random();
+        console.log("高速");
+    } 
+}
+function setreload(timer){ 
+    setTimeout('reloadimg()',timer); 
+}
